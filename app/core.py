cf082a09ddedaac02e99e58882240701426a43a8
Roman Ryzhkov <roman_ryzhkov@internet.ru>
2023-11-20 09:18:17 +0300

    all tasks in  one seesion

--- app/core.py
+++ app/core.py
@@ -1,20 +1,20 @@
-import os
-import json
 import asyncio
-import aiohttp
+import json
+import os
 from typing import List
 
-import requests
+import aiohttp
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
-from app.writers import DBWriter, ExcelWriter
 from app.consts import CITIES_COUNT
+from app.writers import DBWriter, ExcelWriter
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
+API_KEY = os.getenv("API_KEY")
 
 
 class Parser:
@@ -23,50 +23,56 @@ class Parser:
         self.data = pd.DataFrame()
 
     def get_cities(self) -> list[str]:
-        url = 'https://www.worldatlas.com/cities/10-largest-cities-in-the-world.html'
+        url = "https://www.worldatlas.com/cities/10-largest-cities-in-the-world.html"
         response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        cities = [city.select('td')[1].text.strip() for city in soup.select('tr')[1:1 + CITIES_COUNT]]
+        soup = BeautifulSoup(response.content, "html.parser")
+        cities = [
+            city.select("td")[1].text.strip()
+            for city in soup.select("tr")[1:1 + CITIES_COUNT]
+        ]
         self.cities = cities
         return cities
 
-    async def get_weather_data(self, city: str) -> None:
-        async with aiohttp.ClientSession() as session:
-            location_url = f'http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API_KEY}'
-            async with session.get(location_url) as location_response:
-                try:
-                    location_html = await location_response.text()
-                    location = json.loads(location_html)[0]
+    async def get_weather_data(self, session: aiohttp.ClientSession, city: str) -> None:
+        location_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={API_KEY}"
+        async with session.get(location_url) as location_response:
+            try:
+                location_html = await location_response.text()
+                location = json.loads(location_html)[0]
 
-                except Exception as e:
-                    print(f'Не удалось получить координаты города {city}\n{e}')
+            except Exception as e:
+                print(f"Не удалось получить координаты города {city}\n{e}")
 
-                else:
-                    weather_url = (f'https://api.openweathermap.org/data/2.5/forecast?'
-                                   f'lat={location.get("lat")}&lon={location.get("lon")}&'
-                                   f'lang=ru&cnt=24&units=metric&appid={API_KEY}')
+            else:
+                weather_url = (
+                    f"https://api.openweathermap.org/data/2.5/forecast?"
+                    f'lat={location.get("lat")}&lon={location.get("lon")}&'
+                    f"lang=ru&cnt=24&units=metric&appid={API_KEY}"
+                )
 
-                    async with session.get(weather_url) as weather_response:
-                        try:
-                            weather_html = await weather_response.text()
-                            raw_weather_data = json.loads(weather_html).get('list')[::8]
+                async with session.get(weather_url) as weather_response:
+                    try:
+                        weather_html = await weather_response.text()
+                        raw_weather_data = json.loads(weather_html).get("list")[::8]
 
-                        except Exception as e:
-                            print(f'Не удалось получить данные о погоде для города {city}\n{e}')
+                    except Exception as e:
+                        print(
+                            f"Не удалось получить данные о погоде для города {city}\n{e}"
+                        )
 
-                        else:
-                            self._clean_weather_data(raw_weather_data, city)
+                    else:
+                        self._clean_weather_data(raw_weather_data, city)
 
     def _clean_weather_data(self, weather_data: List[dict], city: str) -> None:
         for chunk in weather_data:
             item = {
-                'Город': [city],
-                'Температура': [chunk['main']['temp']],
-                'Влажность': [chunk['main']['humidity']],
-                'Давление': [chunk['main']['pressure']],
-                'Погода': [chunk['weather'][0]['description']],
-                'Скорость ветра': [chunk['wind']['speed']],
-                'Дата': [chunk['dt_txt']],
+                "Город": [city],
+                "Температура": [chunk["main"]["temp"]],
+                "Влажность": [chunk["main"]["humidity"]],
+                "Давление": [chunk["main"]["pressure"]],
+                "Погода": [chunk["weather"][0]["description"]],
+                "Скорость ветра": [chunk["wind"]["speed"]],
+                "Дата": [chunk["dt_txt"]],
             }
             df = pd.DataFrame(item)
             if not self.data.empty:
@@ -75,12 +81,13 @@ class Parser:
                 self.data = df
 
     async def get_weather(self):
-        tasks = []
-        for city in self.get_cities():
-            task = asyncio.create_task(self.get_weather_data(city))
-            tasks.append(task)
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for city in self.get_cities():
+                task = asyncio.create_task(self.get_weather_data(session, city))
+                tasks.append(task)
 
-        await asyncio.gather(*tasks)
+            await asyncio.gather(*tasks)
 
 
 class WeatherForecast(Parser):
--- app/writers.py
+++ app/writers.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 import pandas as pd
 
-STORAGE_DIR = os.path.join(os.getcwd(), 'forecasts')
+STORAGE_DIR = os.path.join(os.getcwd(), "forecasts")
 
 
 class Writer(ABC):
@@ -17,18 +17,18 @@ class Writer(ABC):
 class ExcelWriter(Writer):
     @classmethod
     def save_weather(cls, weather: pd.DataFrame) -> None:
-        path = os.path.join(STORAGE_DIR, 'weather-forecast.xlsx')
+        path = os.path.join(STORAGE_DIR, "weather-forecast.xlsx")
         weather.to_excel(path, index=False)
 
 
 class DBWriter(Writer):
     @classmethod
     def save_weather(cls, weather: pd.DataFrame) -> None:
-        path = os.path.join(STORAGE_DIR, 'db.sqlite3')
+        path = os.path.join(STORAGE_DIR, "db.sqlite3")
         with sqlite3.connect(path) as connection:
             weather.to_sql(
-                name='weather',
+                name="weather",
                 con=connection,
-                if_exists='replace',
+                if_exists="replace",
                 index=False,
             )
